0d7a76fc61d1088bf0cf22c0cb13575e7f29b015
Arthur Keys <arkeys@gmail.com>
2015-04-02 23:38:19 -0700

    Update flog.sh
    log temp in non standard units - assumes a lot of things

--- flog.sh
+++ flog.sh
@@ -1,1 +1,12 @@
+#!/bin/bash
+tout=`TZ='America/Los_Angeles' date +"%Y-%m-%d %H:%M"`
 
+xout=`sudo ~/DHT22 2302 4 | grep Temp`
+until [ $? -eq "0" ]
+do
+  xout=`sudo ~/DHT22 2302 4 | grep Temp`
+done
+
+f=`echo $xout | awk '{print "scale=2;((9/5) * " $3 ") + 32"}' | bc`
+h=`echo $xout | awk {'print $7'}`
+echo $tout " " $f " F " $h "%"
